f715904e3532ce405080776d23a5f60eebe4c2a2
recepcengiz16 <recepcengiz_96@outlook.com>
2022-04-15 15:48:59 +0300

    adding Seed datas

---
+++ NLayer.Repository/Seeds/ProductFeatureSeed.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository.Seeds
+{
+    public class ProductFeatureSeed : IEntityTypeConfiguration<ProductFeature>
+    {
+        public void Configure(EntityTypeBuilder<ProductFeature> builder)
+        {
+            builder.HasData(
+                new ProductFeature { Id=1,Color="Kırmızı",Height=100,Width=50,ProductId=1},
+                new ProductFeature { Id=2,Color="Mavi",Height=110,Width=70,ProductId=2}
+                );
+        }
+    }
+}
---
+++ NLayer.Repository/Seeds/ProductSeed.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository.Seeds
+{
+    public class ProductSeed : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasData(
+                new Product { Id=1,CategoryId=1,Name="Rotring",Price=100,Stock=20,CreatedDate=DateTime.Now},
+                new Product { Id = 2,CategoryId = 1, Name = "Dolma", Price = 200, Stock = 30, CreatedDate = DateTime.Now },
+                new Product { Id = 3, CategoryId = 1, Name = "Tükenmez", Price = 150, Stock = 25, CreatedDate = DateTime.Now },
+                new Product { Id = 4, CategoryId = 2, Name = "Martin Eden", Price = 100, Stock = 20, CreatedDate = DateTime.Now },
+                new Product { Id = 5, CategoryId = 2, Name = "Küçük Prens", Price = 150, Stock = 20, CreatedDate = DateTime.Now }
+                );
+        }
+    }
+}
