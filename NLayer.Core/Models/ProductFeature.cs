d12844a8eed53e2ffc09f1b540646da09481c2d1
recepcengiz16 <recepcengiz_96@outlook.com>
2022-04-16 19:23:45 +0300

    adding dto models

---
+++ NLayer.Core/Models/ProductFeature.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Core.Models
+{
+    public class ProductFeature
+    {
+        public int Id { get; set; }
+        public string Color { get; set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public int ProductId { get; set; }
+
+        //Navigation Property
+        public Product Product { get; set; }
+    }
+}
