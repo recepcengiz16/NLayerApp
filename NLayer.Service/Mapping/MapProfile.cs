91d9197a2a1eaa432dcd4f0fb1669a9366266cda
recepcengiz16 <recepcengiz_96@outlook.com>
2022-04-16 19:34:41 +0300

    mapping

---
+++ NLayer.Service/Mapping/MapProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using NLayer.Core.DTOs;
+using NLayer.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Service.Mapping
+{
+    public class MapProfile:Profile
+    {
+        public MapProfile()
+        {
+            CreateMap<Product, ProductDTO>().ReverseMap(); //iki taraflı çeviririz reverse mapi de eklersek. Yoksa sadece product dto ya çevrilir. APi tarafında da haberdar etmemiz gerekli.
+            CreateMap<Category, CategoryDTO>().ReverseMap();
+            CreateMap<ProductFeature, ProductFeatureDTO>().ReverseMap();
+            CreateMap<ProductUpdateDTO, Product>();
+        }
+    }
+}
