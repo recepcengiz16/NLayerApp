b83d739537ae58ad82cf90d197182d2ffd217300
recepcengiz16 <recepcengiz_96@outlook.com>
2022-04-17 18:28:20 +0300

    reform

--- NLayer.API/Controllers/CategoriesController.cs
+++ NLayer.API/Controllers/CategoriesController.cs
@@ -1,10 +1,23 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using NLayer.Core.Services;
 
 namespace NLayer.API.Controllers
 {
    
     public class CategoriesController : CustomBaseController
     {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet("[action]/{categoryID}")]
+        public async Task<IActionResult> GetSingleCategoryByIdWithProductsAsync(int categoryID)
+        {
+            return CreateActionResult(await _categoryService.GetSingleCategoryByIdWithProductsAsync(categoryID));
+        }
     }
 }
