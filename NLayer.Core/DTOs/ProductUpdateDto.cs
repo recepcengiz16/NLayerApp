623038fca8bb57adc14ee1663bd198f40b2e30d4
recepcengiz16 <recepcengiz_96@outlook.com>
2022-04-17 15:25:41 +0300

    update

--- NLayer.Core/DTOs/ProductUpdateDto.cs
+++ NLayer.Core/DTOs/ProductUpdateDto.cs
@@ -9,9 +9,9 @@ namespace NLayer.Core.DTOs
     public class ProductUpdateDTO
     {
         public int Id { get; set; }
-        public string Color { get; set; }
-        public int Height { get; set; }
-        public int Width { get; set; }
-        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
     }
 }
