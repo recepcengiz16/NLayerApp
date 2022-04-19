d10d51c068bed1a9e87570e75c25ec08c8855e22
recepcengiz16 <recepcengiz_96@outlook.com>
2022-04-19 15:08:02 +0300

    adding product controller

---
+++ NLayer.Web/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NLayer.Core.Models;
+using NLayer.Core.Services;
+
+namespace NLayer.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductService _service;
+
+        public ProductsController(IProductService service)
+        {
+            _service = service;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _service.GetProductsWithCategory());
+        }
+    }
+}
