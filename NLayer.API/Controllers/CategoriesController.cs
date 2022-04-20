e30ac79fff0684c5d8861886bbaadbc2533d218e
recepcengiz16 <recepcengiz_96@outlook.com>
2022-04-20 16:05:55 +0300

    updated

--- NLayer.API/Controllers/CategoriesController.cs
+++ NLayer.API/Controllers/CategoriesController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using NLayer.API.Filters;
+using NLayer.Core.DTOs;
 using NLayer.Core.Services;
 
 namespace NLayer.API.Controllers
@@ -9,11 +11,28 @@ namespace NLayer.API.Controllers
     public class CategoriesController : CustomBaseController
     {
         private readonly ICategoryService _categoryService;
+        private readonly IMapper _mapper;
 
-        public CategoriesController(ICategoryService categoryService)
+        public CategoriesController(ICategoryService categoryService, IMapper mapper)
         {
             _categoryService = categoryService;
+            _mapper = mapper;
         }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var categories = await _categoryService.GetAllAsync();
+
+            var categoriesDTO = _mapper.Map<List<CategoryDTO>>(categories.ToList());
+
+            return CreateActionResult(CustomResponseDTO<List<CategoryDTO>>.Success(200,categoriesDTO));
+
+
+        }
+
+
 
         [HttpGet("[action]/{categoryID}")]
         public async Task<IActionResult> GetSingleCategoryByIdWithProductsAsync(int categoryID)
--- NLayer.Web/Services/ProductApiService.cs
+++ NLayer.Web/Services/ProductApiService.cs
@@ -1,4 +1,6 @@
-﻿namespace NLayer.Web.Services
+﻿using NLayer.Core.DTOs;
+
+namespace NLayer.Web.Services
 {
     public class ProductApiService
     {
@@ -7,6 +9,46 @@
         public ProductApiService(HttpClient client)
         {
             _client = client;
+        }
+
+        public async Task<List<ProductWithCategoryDTO>> GetProductsWithCategoryAsync()
+        {
+            var response = await _client.GetFromJsonAsync<CustomResponseDTO<List<ProductWithCategoryDTO>>>("products/GetProductWithCategory");
+            return response.Data;
+        }
+
+        public async Task<ProductDTO> SaveAsync(ProductDTO product)
+        {
+            var response = await _client.PostAsJsonAsync("products", product);
+
+            if (!response.IsSuccessStatusCode) return null;
+
+            var responseBody = await response.Content.ReadFromJsonAsync<CustomResponseDTO<ProductDTO>>();
+
+            return responseBody.Data; 
+            
+        }
+
+        public async Task<ProductDTO> GetByIdAsync(int id)
+        {
+            var response = await _client.GetFromJsonAsync<CustomResponseDTO<ProductDTO>>($"products/{id}");
+            return response.Data;
+        }
+
+        public async Task<bool> UpdateAsync(ProductDTO product)
+        {
+            var response = await _client.PutAsJsonAsync("products", product);
+
+            return response.IsSuccessStatusCode;
+
+        }
+
+        public async Task<bool> RemoveAsync(int id)
+        {
+            var response = await _client.DeleteAsync($"products/{id}");
+
+            return response.IsSuccessStatusCode;
+
         }
     }
 }
